572c732816ed82dbc0686d1e2107d355d7dc9612
Steve Womble <swomble@aol.com>
2020-05-04 22:03:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,7 +42,7 @@ def self.find_or_create_by_name(name)
       self.find_by_name(name) || self.create_by_name(name)
     end
     
-    def self.aphabetical
+    def self.alphabetical
       @@all.sort_by{|s| s.name}
     end
 
