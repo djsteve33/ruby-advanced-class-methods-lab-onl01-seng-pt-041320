95e96448b3515a59f80f5430aaefcf9e08b53d7c
Steve Womble <swomble@aol.com>
2020-05-04 21:40:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,15 +11,18 @@ class Song
   end
   
 def self.create
-  song = self.new
- song
-end
+   song = self.new
+   song
+  end
   
   def self.new_by_name(name)
     song = self.new
     song.name = name
-    @@all << song
+    song
   end
+  
+  def self.create_by_name(name)
+    
 
 
 end
